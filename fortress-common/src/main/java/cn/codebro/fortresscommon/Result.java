eb849a48583f4cdf2d1da3a17ade52c1eb1e571a
Guowentao <473074764@qq.com>
2022-10-10 10:21:23 +0800

    新增:编写用户CRUD接口

--- fortress-common/src/main/java/cn/codebro/fortresscommon/Result.java
+++ fortress-common/src/main/java/cn/codebro/fortresscommon/Result.java
@@ -5,4 +5,43 @@ package cn.codebro.fortresscommon;
  * @date 2022/10/10
  */
 public class Result {
+    private Integer code;
+    private String message;
+    private Object data;
+
+    private Result(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    private void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    private void setMessage(String message) {
+        this.message = message;
+    }
+
+    public static Result success() {
+        return new Result(200, "执行成功");
+    }
+
+    public static Result success(Object data) {
+        Result result = new Result(200, "执行成功");
+        result.data = data;
+        return result;
+    }
+
+    public static Result fail() {
+        return new Result(400, "系统异常");
+    }
+
 }
--- fortress-system/src/main/java/cn/codebro/fortresssystem/controller/UserController.java
+++ fortress-system/src/main/java/cn/codebro/fortresssystem/controller/UserController.java
@@ -1,7 +1,14 @@
 package cn.codebro.fortresssystem.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import cn.codebro.fortresscommon.Result;
+import cn.codebro.fortresssystem.pojo.FortressSysUser;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author Guo wentao
@@ -11,6 +18,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    private static final int DEFAULT_PAGE_SIZE = 10;
 
+    private final IService<FortressSysUser> userService;
 
+    public UserController(IService<FortressSysUser> userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{page}/{pageSize}")
+    public Result page(@PathVariable int page, @PathVariable(required = false) int pageSize) {
+        return Result.success(userService.page(new Page<>(page, pageSize == 0 ? DEFAULT_PAGE_SIZE : pageSize)));
+    }
+
+    @PostMapping
+    public Result add(FortressSysUser user) {
+        userService.save(user);
+        return Result.success();
+    }
+
+    @PutMapping
+    public Result update(FortressSysUser user) {
+        UpdateWrapper<FortressSysUser> wrapper = new UpdateWrapper<>(user);
+        wrapper.eq("id", user.getId());
+        userService.update(user, wrapper);
+        return Result.success();
+    }
+
+    @DeleteMapping
+    public Result delete(String id) {
+        FortressSysUser user = new FortressSysUser();
+        user.setId(id);
+        userService.removeById(user);
+        return Result.success();
+    }
 }
