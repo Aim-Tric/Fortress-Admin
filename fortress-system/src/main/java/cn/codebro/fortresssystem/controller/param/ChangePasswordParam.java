1fde607d9bb7bdcf886652f7dacceafd4bbe95a2
Gwentao <473074764@qq.com>
2022-12-19 23:08:22 +0800

    1.调整User管理代码结构

---
+++ fortress-system/src/main/java/cn/codebro/fortresssystem/controller/param/ChangePasswordParam.java
@@ -0,0 +1,27 @@
+package cn.codebro.fortresssystem.controller.param;
+
+/**
+ * @author Guo wentao
+ * @project fortress
+ * @date 2022-12-19 14:36:58
+ */
+public class ChangePasswordParam {
+    private String oldPassword;
+    private String newPassword;
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
---
+++ fortress-system/src/main/java/cn/codebro/fortresssystem/exception/SystemBusinessExceptionUtil.java
@@ -0,0 +1,21 @@
+package cn.codebro.fortresssystem.exception;
+
+import cn.codebro.fortresscommon.exception.IllegalBusinessOperationException;
+
+/**
+ * @author Guo wentao
+ * @project fortress
+ * @date 2022-12-18 19:18:41
+ */
+public class SystemBusinessExceptionUtil {
+    private static final Integer DEFAULT_SYSTEM_EXCEPTION_CODE = 500;
+
+    public static IllegalBusinessOperationException dump(String message, String debugMessage) {
+        return new IllegalBusinessOperationException(DEFAULT_SYSTEM_EXCEPTION_CODE, message, debugMessage);
+    }
+
+    public static IllegalBusinessOperationException dump(String message) {
+        return dump(message, "No debug message left. Please check the log file.");
+    }
+
+}
